f3fe0c93fa1647141ec9b8c267e525743c91f987
ZhuRuoLing <jkl-963@live.com>
2023-09-24 21:47:05 +0800

    1.19.2

---
+++ src/main/java/net/zhuruoling/startup_time/Mod.java
@@ -0,0 +1,11 @@
+package net.zhuruoling.startup_time;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Mod implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/zhuruoling/startup_time/mixin/MainMenuMixin.java
@@ -0,0 +1,28 @@
+package net.zhuruoling.startup_time.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.client.toast.ToastManager;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.lang.management.ManagementFactory;
+
+@Mixin(MinecraftClient.class)
+public class MainMenuMixin {
+
+    @Shadow @Final private ToastManager toastManager;
+
+    @Inject(method = "method_29338", at = @At("RETURN"))
+    void inj(CallbackInfo ci){
+        long timeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
+        Text title = Text.of(String.format("游戏启动用时: %.1f秒",timeMillis / 1000.0));
+        Text content = Text.of("打败了全国99.9%的玩家");
+        SystemToast.show(this.toastManager, SystemToast.Type.PERIODIC_NOTIFICATION, title, content);
+    }
+}
