04a987a3dd007dd7c4cdb2eab6916ab7fb1fd02a
ZhuRuoLing <jkl-963@live.com>
2023-09-24 22:38:40 +0800

    Commit Message

--- src/main/java/net/zhuruoling/startup_time/mixin/MainMenuMixin.java
+++ src/main/java/net/zhuruoling/startup_time/mixin/MainMenuMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,13 +17,18 @@ import java.lang.management.ManagementFactory;
 @Mixin(MinecraftClient.class)
 public class MainMenuMixin {
 
+    @Unique
+    private static boolean isStartup = true;
+
     @Shadow @Final private ToastManager toastManager;
 
     @Inject(method = "method_29338", at = @At("RETURN"))
     void inj(CallbackInfo ci){
+        if (!isStartup)return;
         long timeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
         Text title = Text.of(String.format("游戏启动用时: %.1f秒",timeMillis / 1000.0));
         Text content = Text.of("打败了全国99.9%的玩家");
         SystemToast.show(this.toastManager, SystemToast.Type.PERIODIC_NOTIFICATION, title, content);
+        isStartup = false;
     }
 }
