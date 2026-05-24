5d837b1010cd1e4fa11d1f4e0366ea105c5dfc57
ZhuRuoLing <jkl-963@live.com>
2026-05-25 04:29:43 +0800

    26.1

--- src/main/java/icu/takeneko/startup_time/Mod.java
+++ src/main/java/icu/takeneko/startup_time/Mod.java
@@ -1,10 +1,10 @@
 package icu.takeneko.startup_time;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.toast.SystemToast;
+import net.minecraft.client.gui.components.toasts.SystemToast;
 
 public class Mod implements ClientModInitializer {
-    public static final SystemToast.Type TYPE = new SystemToast.Type(10000);
+    public static final SystemToast.SystemToastId TYPE = new SystemToast.SystemToastId(10000);
     @Override
     public void onInitializeClient() {
 
--- src/main/java/icu/takeneko/startup_time/mixin/MainMenuMixin.java
+++ src/main/java/icu/takeneko/startup_time/mixin/MainMenuMixin.java
@@ -1,10 +1,10 @@
 package icu.takeneko.startup_time.mixin;
 
 import icu.takeneko.startup_time.Mod;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.toast.SystemToast;
-import net.minecraft.client.toast.ToastManager;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.gui.components.toasts.ToastManager;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,22 +15,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.lang.management.ManagementFactory;
 
-@Mixin(MinecraftClient.class)
+@Mixin(Minecraft.class)
 public class MainMenuMixin {
 
     @Unique
     private static boolean isStartup = true;
     @Shadow @Final private ToastManager toastManager;
 
-    @Inject(method = "onFinishedLoading", at = @At("RETURN"))
-    void inj(MinecraftClient.LoadingContext loadingContext, CallbackInfo ci) {
+    @Inject(method = "onResourceLoadFinished", at = @At("RETURN"))
+    void inj(Minecraft.GameLoadCookie loadCookie, CallbackInfo ci) {
         if (!isStartup) {
             return;
         }
         long timeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
-        Text title = Text.translatable("startup_time.time", timeMillis/1000.0);
-        Text content = Text.of("");
-        SystemToast.show(this.toastManager, Mod.TYPE, title, content);
+        Component title = Component.translatable("startup_time.time", timeMillis/1000.0);
+        Component content = Component.literal("");
+        SystemToast.add(this.toastManager, Mod.TYPE, title, content);
         isStartup = false;
     }
 }
