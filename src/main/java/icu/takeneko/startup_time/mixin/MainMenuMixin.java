e7b06c45f30916d037e66f92ad4e5f2b5c40e012
ZhuRuoLing <jkl-963@live.com>
2026-09-07 18:37:02 +0800

    Update project to Minecraft 26.2

--- src/main/java/icu/takeneko/startup_time/mixin/MainMenuMixin.java
+++ src/main/java/icu/takeneko/startup_time/mixin/MainMenuMixin.java
@@ -1,10 +1,13 @@
 package icu.takeneko.startup_time.mixin;
 
 import icu.takeneko.startup_time.Mod;
+import net.minecraft.client.GameLoadCookie;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.client.gui.components.toasts.ToastManager;
 import net.minecraft.network.chat.Component;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,21 +19,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.lang.management.ManagementFactory;
 
 @Mixin(Minecraft.class)
-public class MainMenuMixin {
+public abstract class MainMenuMixin {
 
+    @Shadow
+    @Final
+    public Gui gui;
     @Unique
     private static boolean isStartup = true;
-    @Shadow @Final private ToastManager toastManager;
 
     @Inject(method = "onResourceLoadFinished", at = @At("RETURN"))
-    void inj(Minecraft.GameLoadCookie loadCookie, CallbackInfo ci) {
+    void inj(GameLoadCookie loadCookie, CallbackInfo ci) {
         if (!isStartup) {
             return;
         }
         long timeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
         Component title = Component.translatable("startup_time.time", timeMillis/1000.0);
         Component content = Component.literal("");
-        SystemToast.add(this.toastManager, Mod.TYPE, title, content);
+        SystemToast.add(this.gui.toastManager(), Mod.TYPE, title, content);
         isStartup = false;
     }
 }
